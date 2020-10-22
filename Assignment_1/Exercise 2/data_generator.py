264d1d2bb6fc418f6d5521998341f08f2d59bbf9
JP <joaoteixeira@eng.ci.ufpb.br>
2020-10-22 19:27:30 -0300

    Add: Exercise 2 -> Data Generator and problem A

--- Assignment_1/Exercise 2/data_generator.py
+++ Assignment_1/Exercise 2/data_generator.py
@@ -1,26 +1,57 @@
 import numpy as np
 
+def f(x):
+    y = math.sin(math.pi * x) / (math.pi * x)
+    return y
 
-def generate_data(data_size, train_test_split=0.1):
-    """
-    Args : 
-        data_size = number of instances to be produced
-        train_val_split = percentage of data to be used on validation split
-    Return:
-        
-    """
-    X = np.zeros((data_size, 2))
-    y = np.zeros((data_size, 1))
-
-    for i in range(data_size):
-        X[i] = np.random.randint(0,2,2)
-        if X[i][0] != X[i][1]:
-            y[i] = 1
-        X[i] += np.random.uniform(-0.1,0.1, 2)
+class DataGenerator():
     
-    test_split = round(data_size * (1 - train_test_split))
+    def generate_problem_A(self, opt = 'std'):
+        """
+        Function that generates the data for the XOR logic function:
+        
+           X | Y |
+        A| 0 | 0 | 0
+        B| 0 | 1 | 1
+        C| 1 | 0 | 1
+        D| 1 | 1 | 0
+        
+        
+        """
+        
+        if opt == 'std':
 
-    X_train, y_train = X[:test_split], y[:test_split]
-    X_test, y_test = X[test_split:], y[test_split:]
+            X = np.array([[0,0], [0,1], [1,0], [1,1]])
+            y = np.array([[1,0],[0,1],[0,1],[1,0]])
 
-    return X_train, X_test, y_train, y_test
+            for i in [0,1,2,3]:
+                print(f'{X[i]} => {y[i]}')
+
+            X_train, X_test, y_train, y_test = X, X, y, y
+
+            return X_train, X_test, y_train, y_test
+        
+        elif opt == 'noise':
+            
+            print('under construction')
+            
+        else:
+            
+            print('invalid option!')
+             
+        
+        
+        
+        
+        
+    def generate_problem_B(self, n_samples = 1000, train_val_split = 0.1):
+        
+        X = np.random.uniform(0.01, 5, n_samples)
+        y = np.array([f(Xi) for Xi in X])
+        
+        
+        val_split = round(n_samples * (1 - train_val_split))
+
+        X_train, y_train = X[:val_split].T, y[:val_split].T
+        X_val, y_val = X[val_split:].T, y[val_split:].T
+        return X_train, X_val, y_train, y_val
