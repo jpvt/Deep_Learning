feb1946541d88ed3103227c243e9614da13e6eec
void_zero <wallace.lucena@hotmail.com>
2020-10-23 13:31:51 -0300

    Corrected the prediction graph

--- Assignment_1/Exercise_4/data_generator.py
+++ Assignment_1/Exercise_4/data_generator.py
@@ -8,9 +8,9 @@ def generate_data(interval: tuple, k_steps: int, step_size: float):
     y = []
     
     for n_step in range(interval[0], interval[1]):
-        # Aqui, pegamos os K passos anteriores
+        # Here we get the last K steps, making the interval [n_step - K, n_step)
         X.append(np.array(list(map(f, np.arange(n_step - k_steps, n_step, step_size)))))
-        # Já aqui, pegamos os 3 passos seguintes, que representariam a saída de nossa rede
+        # And here, we get the following 3 steps [n_step, n_step+3), that represent the output of our network
         y.append(np.array(list(map(f, np.arange(n_step, n_step + 3, step_size)))))
     
     return X, y
